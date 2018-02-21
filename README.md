dacb07bd71578b7c38dddd80f6ca2c8621a83dbb
Douglas Quintanilha Barbosa Ferreira | Stone <dbferreira@stone.com.br>
2018-02-21 18:06:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stop Windows Service VSTS task
+
+A small task to stop a Windows Service that is running on the same machine as the VSTS agent that executes this task.
+
+Useful to use when working with Deployment Groups in VSTS.
---
+++ StopWindowsService.ps1
@@ -0,0 +1,79 @@
+[CmdletBinding()]
+param([switch]$dotSourceOnly)
+function Stop-WindowsService($serviceName, $timeout) {
+    $status = "Stopped"
+    $service = Get-Service $serviceName
+    Stop-Service $service -Force
+    try{
+        Write-Host "Stop timeout: $timeout"
+        $service.WaitForStatus($status, $timeout)
+        return $True
+    }catch{
+        Write-Host "Timeout reached."            
+        Write-Debug $_
+        return $False
+    }
+}
+
+function Kill-WindowsService ($serviceName) {
+    $service = Get-Service $serviceName
+    $serviceObject = New-Object -TypeName "System.Management.ManagementObject" -ArgumentList "Win32_service.Name='$($service.name)'"
+    $serviceObject
+    $servicePid = $serviceObject.GetPropertyValue("ProcessId")
+    if($servicePid -ne 0) {
+        if(Get-Process -Id $servicePid){
+            Write-Host "Process $servicePid still running, killing it..."
+            Stop-Process $servicePid -Force
+        }
+        Write-Host "Service $serviceName killed."
+    }else{
+        Write-Host "[Kill-WindowsService] Process not found for service $serviceName."
+    }
+}
+
+function Test-ServiceExists($serviceName) {
+    $service = Get-Service $serviceName
+
+    if($service){
+        Write-Host "Service $($service.name) was found."
+        return $True
+    }else{        
+        return $False
+    }
+}
+function Main () {
+    # For more information on the VSTS Task SDK:
+    # https://github.com/Microsoft/vsts-task-lib
+    Trace-VstsEnteringInvocation $MyInvocation
+    try {
+        
+        $serviceName = Get-VstsInput -Name "ServiceName" -Require
+        $shouldKillService = Get-VstsInput -Name "KillService" -Require
+        $timeout = Get-VstsInput -Name "Timeout" -Require
+        $SkipWhenServiceDoesNotExists = Get-VstsInput -Name "SkipWhenServiceDoesNotExists" -Require
+
+        if(Test-ServiceExists $serviceName){
+            $serviceStopped = Stop-WindowsService -serviceName $serviceName -timeout $timeout
+
+            if((-not $serviceStopped) -and $shouldKillService){
+                Kill-WindowsService $serviceName
+            }else{
+                throw "The service $serviceName could not be stopped and kill service option was disabled."
+            }
+
+        }else{
+            if($SkipWhenServiceDoesNotExists){
+                Write-Host "The service $serviceName does not exist. Skipping this task."         
+            }else{
+                throw "The service $serviceName does not exist. Please check the service name on the task configuration."
+            }
+        }        
+        
+    } finally {
+        Trace-VstsLeavingInvocation $MyInvocation
+    }
+}
+
+if($dotSourceOnly -eq $false){
+    Main
+}
